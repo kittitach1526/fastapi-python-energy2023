b6f347e933a914f287ea7f90493192b2d47c267f
Glenda <124149468+kittitach1526@users.noreply.github.com>
2026-04-07 11:38:13 +0700

    add debuger

--- main.py
+++ main.py
@@ -21,5 +21,9 @@ app.include_router(flow_router)
 app.include_router(status_router)
 
 
+@app.get("/")
+async def root():
+    return {"msg": "ok"}
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
